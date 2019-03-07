50a29b37bfb405dc1276686cbd6ea6513de00f77
huara <47011132+huara@users.noreply.github.com>
2019-03-06 22:12:19 -0500

    OOPrinciples Stated

--- src/midtermreview/VendingMachineSimulator.java
+++ src/midtermreview/VendingMachineSimulator.java
@@ -2,10 +2,16 @@
  * Raymond Hua
  * Student ID: 991 485 288
  * SYST10199 - Web Programming
+ * Removed the scanner.
+ * The VendingMachineItems classes are loosely coupled because the simulator is only focusing on being the interface for the user, while 
+ * VendingMachineItem classs is the system that provides the items and specfies the prices. Because they are polarizing functionalites in 
+ * the system, they become dependent on each other because they each other speciality in order for the entire program function as a unit. 
+ * Also, because each class as different responsibilites, then the entire program is highly cohesive since each class is only performing tasks
+ * related to it's concept/main responsibility, and will therefore allow for more flexibility since adding features won't change the entire 
+ * implementation of the VendingMachine object. 
  */
 package midtermreview;
 
-import java.util.Scanner;
 import static midtermreview.VendingMachineItem.candies;
 
 /**
@@ -14,8 +20,7 @@ import static midtermreview.VendingMachineItem.candies;
  */
 public class VendingMachineSimulator {
      public static void main(String[] args)
-    {
-        Scanner sc = new Scanner(System.in);
+     {
         System.out.println("Welcome to the vendin machine, here is a list of the possible candies");
         for(int i=0; i<candies.length; i++)
         {
